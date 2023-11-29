9514a2c777397c6dd7c86c24feb597e83fc99f34
sajid-M <sajid.m@experionglobal.com>
2023-11-29 12:57:30 +0530

    first commit

---
+++ src/app.controller.ts
@@ -0,0 +1,53 @@
+import {
+  BadRequestException,
+  Body,
+  Controller,
+  Post,
+  Res,
+} from '@nestjs/common';
+import { AppService } from './app.service';
+import * as bcrypt from 'bcrypt';
+import { JwtService } from '@nestjs/jwt';
+import { Response } from 'express';
+@Controller('api')
+export class AppController {
+  constructor(
+    private readonly appService: AppService,
+    private jwtService: JwtService,
+  ) {}
+
+  @Post('register')
+  async register(
+    @Body('name') name: string,
+    @Body('email') email: string,
+    @Body('password') password: string,
+  ) {
+    const hashedPassword = await bcrypt.hash(password, 12);
+    return this.appService.create({
+      name,
+      email,
+      password: hashedPassword,
+    });
+  }
+
+  @Post('login')
+  async login(
+    @Body('email') email: string,
+    @Body('password') password: string,
+    @Res({ passthrough: true }) response: Response,
+  ) {
+    const user = await this.appService.findOne({ where: { email } });
+    if (!user) {
+      throw new BadRequestException('invalid credentials');
+    }
+    const isValid = await bcrypt.compare(password, user.password);
+    if (!isValid) {
+      throw new BadRequestException('invalid credentials');
+    }
+    const jwt = await this.jwtService.signAsync({ id: user.id });
+    response.cookie('jwt', jwt, { httpOnly: true });
+    return {
+      message: 'success',
+    };
+  }
+}
